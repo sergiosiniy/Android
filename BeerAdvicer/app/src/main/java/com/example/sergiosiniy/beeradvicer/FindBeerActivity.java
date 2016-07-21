ea45466f9b3264851a2e51110ef2950a4f95b285
Serhii <serhii.synohub@gmail.com>
2016-07-21 16:44:40 +0300

    add method to BeerAdvicer activity

--- BeerAdvicer/app/src/main/java/com/example/sergiosiniy/beeradvicer/FindBeerActivity.java
+++ BeerAdvicer/app/src/main/java/com/example/sergiosiniy/beeradvicer/FindBeerActivity.java
@@ -2,6 +2,7 @@ package com.example.sergiosiniy.beeradvicer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FindBeerActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class FindBeerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view){
+
     }
 }
