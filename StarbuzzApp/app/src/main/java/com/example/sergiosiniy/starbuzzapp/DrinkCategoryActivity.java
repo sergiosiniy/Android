333625841705d982ee2a2222583194e2b6d0dd12
Serhii <serhii.synohub@gmail.com>
2016-09-21 17:01:19 +0300

    O'Railly part6 adapters

---
+++ StarbuzzApp/app/src/main/java/com/example/sergiosiniy/starbuzzapp/DrinkCategoryActivity.java
@@ -0,0 +1,31 @@
+package com.example.sergiosiniy.starbuzzapp;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DrinkCategoryActivity extends ListActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ListView listView = getListView();
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                Drink.drinks);
+        listView.setAdapter(listAdapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView listView,
+                                View itemView,
+                                int position,
+                                long id) {
+        Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class)
+            .putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
+        startActivity(intent);
+    }
+}
---
+++ StarbuzzApp/app/src/main/java/com/example/sergiosiniy/starbuzzapp/TopLevelActivity.java
@@ -0,0 +1,34 @@
+package com.example.sergiosiniy.starbuzzapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class TopLevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_level);
+        AdapterView.OnItemClickListener listViewListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(listViewListener);
+
+    }
+
+
+
+
+
+}
